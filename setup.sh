c70de5ad31a72d00f6234b4e243d6dbe6bd2db08
Brunorwell12 <87946702+Brunorwell@users.noreply.github.com>
2024-10-18 20:15:59 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"brunorwell12@outlook.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
